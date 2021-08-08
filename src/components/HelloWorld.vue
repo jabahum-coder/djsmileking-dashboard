cb455e7913a28ce0653a72bcceee93e27e30bf0b
Jabar Jeremy <8259660-jabahum-coder@users.noreply.gitlab.com>
2021-08-08 14:10:15 +0300

    Edited

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <b-card-group deck>
+    <b-card-group>
       <b-card
         footer="$150 /night"
         footer-tag="footer"
@@ -15,18 +15,6 @@
         <b-card-text>
           Good vibes of the city, catch the sunset from the rooftop.
         </b-card-text>
-        <b-list-group>
-          <b-list-group-item>Wifi, Kitchen, Heating</b-list-group-item>
-          <b-list-group-item>Free Parking lot</b-list-group-item>
-          <b-list-group-item>Gym and Swimming pool</b-list-group-item>
-        </b-list-group>
-        <br />
-        <b-button href="#" variant="info" v-b-modal.modal-1
-          >Book a stay</b-button
-        >
-        <b-modal id="modal-1" title="Payment Confirmation">
-          <p class="my-4">You have just booked your Vuebnb</p>
-        </b-modal>
       </b-card>
     </b-card-group>
   </div>
